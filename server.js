d48e2bb0b8a1c86b5175cbe8d7f82908394071f1
Wen Gong <gongwen1201@gmail.com>
2022-10-12 10:51:35 +1100

    .env url upload

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000; 
-
-
+const PORT = process.env.PORT || 3000;
 
 const cors = require('cors');
 // Use this CORS package as part of the Express
